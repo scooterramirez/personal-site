18ad50743a88b843f38726ccb10abcf88d42c8be
Gabe Whaley <whaleyg@Gabes-MacBook-Pro-2.local>
2017-05-29 22:02:28 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,394 @@
+document.getElementById('gabe_link').onmouseover= function () {
+
+	var butt = document.getElementById("gabe_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("friendswholiketrump_description");
+	wat.style.display = "none";
+
+	var heh = document.getElementById("reginald_description");
+	heh.style.display = "none";
+
+	var girl = document.getElementById("makeovr_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("toothpick_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("wingman_description");
+	lord.style.display = "none";
+
+	var wfa = document.getElementById("worstfastagency_description");
+	wfa.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('fmlist_link').onmouseover= function () {
+	
+	var butt = document.getElementById("fmlist_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("friendswholiketrump_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var heh = document.getElementById("reginald_description");
+	heh.style.display = "none";
+
+	var girl = document.getElementById("makeovr_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("toothpick_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("wingman_description");
+	lord.style.display = "none";
+
+	var wfa = document.getElementById("worstfastagency_description");
+	wfa.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+
+}
+
+document.getElementById('friendswholiketrump_link').onmouseover= function () {
+
+	var butt = document.getElementById("friendswholiketrump_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var heh = document.getElementById("reginald_description");
+	heh.style.display = "none";
+
+	var girl = document.getElementById("makeovr_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("toothpick_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("wingman_description");
+	lord.style.display = "none";
+
+	var wfa = document.getElementById("worstfastagency_description");
+	wfa.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('reginald_link').onmouseover= function () {
+
+	var butt = document.getElementById("reginald_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("makeovr_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("toothpick_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("wingman_description");
+	lord.style.display = "none";
+
+	var wfa = document.getElementById("worstfastagency_description");
+	wfa.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('makeovr_link').onmouseover= function () {
+
+	var butt = document.getElementById("makeovr_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("reginald_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("toothpick_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("wingman_description");
+	lord.style.display = "none";
+
+	var wfa = document.getElementById("worstfastagency_description");
+	wfa.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('toothpick_link').onmouseover= function () {
+
+	var butt = document.getElementById("toothpick_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("reginald_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("makeovr_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("wingman_description");
+	lord.style.display = "none";
+
+	var wfa = document.getElementById("worstfastagency_description");
+	wfa.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('wingman_link').onmouseover= function () {
+
+	var butt = document.getElementById("wingman_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("reginald_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("makeovr_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("toothpick_description");
+	lord.style.display = "none";
+
+	var wfa = document.getElementById("worstfastagency_description");
+	wfa.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('worstfastagency_link').onmouseover= function () {
+
+	var butt = document.getElementById("worstfastagency_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("reginald_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("makeovr_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("toothpick_description");
+	lord.style.display = "none";
+
+	var booger = document.getElementById("imessage_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+
+}
+
+document.getElementById('imessage_link').onmouseover= function () {
+
+	var butt = document.getElementById("imessage_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("reginald_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("makeovr_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("toothpick_description");
+	lord.style.display = "none";
+
+	var wingman = document.getElementById("wingman_description");
+	wingman.style.display = "none";
+
+	var booger = document.getElementById("worstfastagency_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("kicking4hunger_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('kicking4hunger_link').onmouseover= function () {
+
+	var butt = document.getElementById("kicking4hunger_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("reginald_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("makeovr_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("toothpick_description");
+	lord.style.display = "none";
+
+	var wingman = document.getElementById("wingman_description");
+	wingman.style.display = "none";
+
+	var booger = document.getElementById("worstfastagency_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("imessage_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("squad_description");
+	kh.style.display = "none";
+}
+
+document.getElementById('squad_link').onmouseover= function () {
+
+	var butt = document.getElementById("squad_description");
+	butt.style.display = "block";
+
+	var lol = document.getElementById("fmlist_description");
+	lol.style.display = "none";
+
+	var wat = document.getElementById("gabe_description");
+	wat.style.display = "none";
+
+	var see = document.getElementById("friendswholiketrump_description");
+	see.style.display = "none";
+
+	var girl = document.getElementById("reginald_description");
+	girl.style.display = "none";
+
+	var hoard = document.getElementById("makeovr_description");
+	hoard.style.display = "none";
+
+	var lord = document.getElementById("toothpick_description");
+	lord.style.display = "none";
+
+	var wingman = document.getElementById("wingman_description");
+	wingman.style.display = "none";
+
+	var booger = document.getElementById("worstfastagency_description");
+	booger.style.display = "none";
+
+	var imessage = document.getElementById("imessage_description");
+	imessage.style.display = "none";
+
+	var kh = document.getElementById("kicking4hunger_description");
+	kh.style.display = "none";
+}
